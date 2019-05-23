5a97494626bce360df299083c634a50b0db11160
koencaprino <koencaprino@hotmail.com>
2019-05-23 09:53:11 +0200

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,32 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+    0,
+    9,
+    12,
+    14,
+    15,
+    17,
+    18,
+    24,
+    25.8,
+    31,
+    32,
+    33.1,
+    34,
+    36.4,
+    39,
+    43.2,
+    44.8,
+    48.4,
+    51.9,
+    55,
+    57.5,
+    60.5,
+    63,
+    65,
+    69,
+    71,
+    72.5,
+    86
+];
